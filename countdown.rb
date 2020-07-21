e10ca902a371a45f081e3d56ec9118d74ae2d0f5
Caleb R <cjr4912@gmail.com>
2020-07-21 14:43:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+def countdown(time)
+  while time < 10 
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    
+  puts "HAPPY NEW YEAR!"
 
-def countdown
+  end
+end
+
+def countdown_with_sleep(time)
+  while time < 10
+  sleep 1.seconds until time = 0 
+  puts "#{time} SECOND(S)!"
+  time -= 1
+  
+  puts "HAPPY NEW YEAR!"
+  
+  end
 end
